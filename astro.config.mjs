0b3529743629cb8a150b2d1ef7e9291b2a99afa4
edwardsifi <edwardsifi0@gmail.com>
2024-12-13 20:27:37 -0600

    feat(workwlows): adds yml configs for deplots to github actions
    this also has configurations for the main domian of the github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://devtoolsystems.github.io",
+  base: 'web',
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
