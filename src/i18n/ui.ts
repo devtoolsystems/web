3f20fbbb608daef8422fb25a21510cab363e7e5e
edwardsifi <edwardsifi0@gmail.com>
2024-12-23 23:58:09 -0600

    feat(pages): adds configurarion for i18n
    and also it adds some examples of the implementation

---
+++ src/i18n/ui.ts
@@ -0,0 +1,21 @@
+export const languageList = {
+    es: "Español",
+    en: "English"
+}
+
+export const labels = {
+    es: {
+        "nav.home": "Inicio",
+        "nav.about": "Sobre nosotros",
+        "nav.about.title": "Sobre nosotros",
+        "home.title": "Bienvenidos a Programación en español",
+        "about.title": "Acerca de la comunidad",
+    },
+    en: {
+        "nav.home": "Home",
+        "nav.about": "About",
+        "nav.about.title": "About us",
+        "home.title": "Welcome to Programación en español",
+        "about.title": "About our community",
+    },
+};
